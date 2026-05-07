40c7b016e96abcc1f381f14d56c6d407047f87b0
YAGOPOP <ttatarinov50@gmail.com>
2026-05-07 22:20:54 +0300

    removed some unnecessary stuff

--- plugin-initiators/ejs.js
+++
@@ -1,6 +0,0 @@
-module.exports=(fastify) => {
-  fastify.register(require('@fastify/view'), {
-    engine: { ejs: require('ejs') },
-    root: './views'
-  });
-}
--- public/js/el.js
+++
@@ -1,7 +0,0 @@
-function el(tag, attrs={}, children=[], listeners={}, ns=null) {
-	const res=ns?document.createElementNS(tag, ns):document.createElement(tag);
-	for(const key in attrs)res.setAttribute(key, attrs[key]);
-	for(const child of children)res.append(child);
-	for(const i in listeners)res.addEventListener(i,listeners[i]);
-	return res;
-}
